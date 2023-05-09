a4e6ce5c35b7d1ae1dfa5daee690ec91b0150957
Stefan Berggren <nsg@nsg.cc>
2023-05-09 17:04:56 +0200

    WIP

--- query_db.py
+++ query_db.py
@@ -1,25 +1,67 @@
-import sys
 import json
+import argparse
+import os
 
+import numpy as np
 from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
 
-#DB_FILE = "stockholm.db"
-DB_FILE = "sweden.db.json"
-
-def comp(shape: list, point: tuple) -> bool:
-    polygon = Polygon(shape)
-    point = Point(point[0], point[1])
-
-    return polygon.contains(point)
-
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog='query-db')
+    parser.add_argument('-w', '--world-file', required=True)
+    parser.add_argument('--longitude', required=True, type=float)
+    parser.add_argument('--latitude', required=True, type=float)
+    args = parser.parse_args()
 
-    point = (sys.argv[1], sys.argv[2])
+    point = Point(args.longitude, args.latitude)
 
-    with open(DB_FILE, 'r') as file:
-        db = json.load(file)
+    #
+    # Open world file and look if our point is inside a polygon.
+    # Each polygon in the world file will point to a region specific file
+    # This will save all matched regions in an array
+    #
 
-    for name, level, data in db:
-        if comp(data, point):
-            print(name, level)
+    with open(args.world_file, 'r') as file:
+        world = json.load(file)
+
+    regions = []
+    for data, region, admin_levels in world:
+        polygon = Polygon(data)
+        with np.errstate(invalid="ignore"):
+            if polygon.contains(point):
+                regions.append((region, admin_levels))
+
+    #
+    # Loop over the matched regions
+    #
+
+    print(regions)
+
+    response = []
+    for region, admin_levels in regions:
+        path_dir = os.path.dirname(args.world_file)
+        region_file_full = f"{path_dir}/{region}"
+        region_file_simple = region_file_full.replace(".json.db", ".simplified.json.db")
+
+        if os.path.exists(region_file_simple):
+            region_file = region_file_simple
+        else:
+            region_file = region_file_full
+
+        with open(region_file, 'r') as file:
+            region_data = json.load(file)
+
+        for name, level, data in region_data:
+            polygon = Polygon(data)
+            with np.errstate(invalid="ignore"):
+                if polygon.contains(point):
+                    if level == admin_levels[0]:
+                        admin_level1 = name
+                    elif level == admin_levels[1]:
+                        admin_level2 = name
+                    elif len(admin_levels) > 2 and level == admin_levels[2]:
+                        admin_level3 = name
+
+        response.append({"admin1": admin_level1, "admin2": admin_level2, "admin3": admin_level3})
+
+    print(response)
